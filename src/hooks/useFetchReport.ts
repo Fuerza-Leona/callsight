bbf333f7d148eae51d8597b615bf68df54785e59
miguelmendozajr <miguelmendozaj20@gmail.com>
2025-06-06 06:18:05 -0600

    fix: report redirect

--- src/hooks/useFetchReport.ts
+++ src/hooks/useFetchReport.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import api from '@/utils/api';
+import { useRouter } from 'next/navigation';
 
 export interface ReportResponse {
   success: boolean;
@@ -21,13 +22,14 @@ export const useFetchReport = () => {
   const [report, setReport] = useState<string>();
   const [loadingReport, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   const fetchReport = async () => {
     setLoading(true);
     try {
       const response = await api.post<ReportResponse>(`reports/monthly`, {});
       setReport(response.data.report.file_url || '');
-      window.open(response.data.report.file_url, '_blank');
+      router.push(response.data.report.file_url);
     } catch {
       setError('Error fetching report');
     } finally {
